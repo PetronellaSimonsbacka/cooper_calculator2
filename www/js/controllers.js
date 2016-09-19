c4a4d638d21151767146fd8e1d191cd22bc78c01
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-19 19:23:34 +0200

    Implemented Imperial

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,7 +8,12 @@ angular.module('starter.controllers', [])
       weight: $scope.data.weight,
       height: $scope.data.height
     });
-    person.calculate_bmi_met();
+    var selectObject = document.getElementById('metric_radio');
+    if (selectObject.checked) {
+       person.calculate_bmi_met();
+    } else {
+      person.calculate_bmi_imp();
+    }
     $scope.person = person;
   };
 })
