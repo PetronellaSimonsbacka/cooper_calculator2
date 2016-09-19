def53afea0644365cec74314c8d955454795357c
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-19 14:43:17 +0200

    Taking in person.js and bmi_calculator files from older project, not calculation

---
+++ www/js/person.js
@@ -0,0 +1,14 @@
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+}
+
+Person.prototype.calculate_bmi = function() {
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+};
+
+Person.prototype.calculate_imperial = function() {
+  calculator = new BMICalculator2();
+  calculator.imperial_bmi(this);
+};
